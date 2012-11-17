18be5d1da9ffd4d525b3b622a07cd2d10c7d9b22
npadierna <npadierna@gmail.com>
2012-11-17 14:28:08 -0500

    Commit from Netbeans IDE.

--- AppletClient/Ludens/MapLudens/src/co/edu/udea/ludens/applet/util/LudensConstants.java
+++ AppletClient/Ludens/MapLudens/src/co/edu/udea/ludens/applet/util/LudensConstants.java
@@ -17,6 +17,7 @@ public class LudensConstants {
     public static final String URL_BASE = "urlBase";
     public static final String ELEMENT_TAG = "co.edu.udea.ludens.domain.Element";
     public static final String MESSAGE_EVENT_TAG = "co.edu.udea.ludens.util.MessageEvent";
+    public static final String NO_IMAGE_FILE = "no-image.png";
     public static final String POPULATION_TAG = "co.edu.udea.ludens.domain.Population";
     public static final String FETCHED_ELEMENTS = "Factores y Materiales actualizados desde el servidor";
     public static final String DONT_FETCH_ELEMENTS = "No se pudo leer factores y materiales desde el servidor. Verifique si tiene acceso a internet";
